e8121fd6f057d73ed54081341758b531c8ab817a
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-21 02:12:38 -0400

    testing drop, take, take-while, drop-while

--- playground/src/chapter_4.clj
+++ playground/src/chapter_4.clj
@@ -71,12 +71,56 @@
 (reduce + 0 (range 5))
 ;; initial value is {}. the associated new-map is returned, and passed into the
 ;; next iteration of the fn. meanwhile, the next #{key val} pair is passed.
- (reduce (fn [new-map
-              [key val]]
-           (assoc new-map key (inc val)))
-         {}
-         {:max 30
-          :min 10}) ;; => {:max 31, :min 11}
+(reduce (fn [new-map
+             [key val]]
+          (assoc new-map key (inc val)))
+        {}
+        {:max 30
+         :min 10}) ;; => {:max 31, :min 11}
+
+;; take, drop, take-while, and drop-while
+;;
+;; take takes everything matching the predicate
+;; drop drops everything matching the predicate
+;;
+
+;; takes 3 items from the next parameter, first `n` of the lazy sequence
+;; returned by range
+(take 3 (range 1 1000)) ;; => (1 2 3)
+(drop 3 [1 2 3 4 5])
+
+(def food-journal
+  [{:month 1 :day 1 :human 5.3 :critter 2.3}
+   {:month 1 :day 2 :human 5.1 :critter 2.0}
+   {:month 2 :day 1 :human 4.9 :critter 2.1}
+   {:month 2 :day 2 :human 5.0 :critter 2.5}
+   {:month 3 :day 1 :human 4.2 :critter 3.3}
+   {:month 3 :day 2 :human 4.0 :critter 3.8}
+   {:month 4 :day 1 :human 3.7 :critter 3.9}
+   {:month 4 :day 2 :human 3.7 :critter 3.6}])
+
+(take-while #(< (:month %) 3) food-journal)
+  ;; => ({:month 1 :day 1 :human 5.3 :critter 2.3}
+  ;;     {:month 1 :day 2 :human 5.1 :critter 2.0}
+  ;;     {:month 2 :day 1 :human 4.9 :critter 2.1}
+  ;;     {:month 2 :day 2 :human 5.0 :critter 2.5})
+
+(drop-while #(< (:month %) 3) food-journal)
+  ;; => ({:month 3 :day 1 :human 4.2 :critter 3.3}
+  ;;     {:month 3 :day 2 :human 4.0 :critter 3.8}
+  ;;     {:month 4 :day 1 :human 3.7 :critter 3.9}
+  ;;     {:month 4 :day 2 :human 3.7 :critter 3.6})
+
+;; drop months less than 2, then take months less than 4 from that list(2-3) 
+(take-while #(< (:month %) 4)
+            (drop-while #(< (:month %) 2) food-journal))
+  ;; => ({:month 2 :day 1 :human 4.9 :critter 2.1}
+  ;;     {:month 2 :day 2 :human 5.0 :critter 2.5}
+  ;;     {:month 3 :day 1 :human 4.2 :critter 3.3}
+  ;;     {:month 3 :day 2 :human 4.0 :critter 3.8})
+
+;; filter and some
+;;
 
 ;; main
 ;;
