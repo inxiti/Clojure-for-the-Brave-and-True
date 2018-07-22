7eb69c1a697eaec4fd1d2fde282b2775aa2850d8
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-22 00:15:53 -0400

    some filter/some examples

--- playground/src/chapter_4.clj
+++ playground/src/chapter_4.clj
@@ -122,6 +122,20 @@
 ;; filter and some
 ;;
 
+(filter #(< (:human %) 5) food-journal)
+  ;; => ({:month 2 :day 1 :human 4.9 :critter 2.1}
+  ;;     {:month 3 :day 1 :human 4.2 :critter 3.3}
+  ;;     {:month 3 :day 2 :human 4.0 :critter 3.8}
+  ;;     {:month 4 :day 1 :human 3.7 :critter 3.9}
+  ;;     {:month 4 :day 2 :human 3.7 :critter 3.6})
+
+(some #(> (:critter %) 5) food-journal) ;; => nil
+
+(some #(> (:critter %) 3) food-journal) ;; => true
+
+;; using a set as the predicate
+(some #{:fred} [:fred :wilma]) ;; => :fred
+
 ;; main
 ;;
 
