82ee6c1d3afb500f01bac3c2a23e87f89980fbde
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-25 00:31:33 -0400

    initial stubbing and implementing fpwd

--- fpwd/src/fpwd/core.clj
+++ fpwd/src/fpwd/core.clj
@@ -2,4 +2,15 @@
 
 (def filename "suspects.csv")
 
-(slurp filename)
+(def vamp-keys [:name :glitter-index])
+
+(defn str->int
+  [str]
+  (Integer. str))
+
+(def conversions {:name identity
+                  :glitter-index str->int})
+
+(defn convert
+  [vamp-key value]
+  ((get conversions vamp-key) value))
