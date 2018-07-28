5d8b1749271293a80c822aea3c856023ea41b996
inxiti <32788205+inxiti@users.noreply.github.com>
2018-07-28 01:26:34 -0400

    sum fn, recursive solution

--- playground/src/chapter_5.clj
+++ playground/src/chapter_5.clj
@@ -17,26 +17,37 @@
     "You get a raise!"
     "Better luck next year!"))
 
+;; referentially transparent, same result for same argument
+(defn analysis
+  [text]
+  (str "Character count: " (count text)))
+
 ;; not referentially transparent, the contents of the file can change outside of
 ;; this function.)
 (defn analyze-file
   [filename]
   (analysis (slurp filename)))
 
-;; referentially transparent, same result for same argument
-(defn analysis
-  [text]
-  (str "Character count: " (count text)))
-
 ;; def defines a global variable
 (def great-baby-name "Rosanthony")
 great-baby-name ;; => "Rosanthony"
 
 ;; let defines a lexical scope variable bound within its parentheses
-(let [great-baby-name "Bloodthunder"
-      great-baby-name]) ;; => "Bloodthunder"
+(let [great-baby-name "Bloodthunder"]
+  great-baby-name) ;; => "Bloodthunder"
 
 great-baby-name ;; => "Rosanthony"
+
+(defn sum
+  ([vals]
+   (sum vals 0))
+  ([vals accumulating-total]
+   (if (empty? vals)
+     accumulating-total
+     #_(sum (rest vals) (+ (first vals) accumulating-total))
+     (recur (rest vals) (+ (first vals) accumulating-total)))))
+
+(sum [0 1 2 3 4 5 15]) ;; => 30
 
 (defn -main
   []
