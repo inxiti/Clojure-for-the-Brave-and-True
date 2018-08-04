b76e64cb39e0fc36188ecdd4ca7f6b9d88b69749
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-04 00:33:35 -0400

    defined the remaining rendering/printing functions

--- pegthing/src/pegthing/core.clj
+++ pegthing/src/pegthing/core.clj
@@ -216,31 +216,55 @@
 
 ;; put all lower-case alphabetical letters in a (lazy) sequence
 (def alpha-start 97)
-(def alpha-end   123)
-(def letters     (map (comp str char) (range alpha-start alpha-end)))
-(def pos-chars   3)
+(def alpha-end 123)
+(def letters (map (comp str char) (range alpha-start alpha-end)))
+(def pos-chars 3)
+
+(def ansi-styles
+  {:red   "[31m"
+   :green "[32m"
+   :blue  "[34m"
+   :reset "[0m"})
+
+(defn ansi
+  "Produce a string which will apply an ANSI style."
+  [style]
+  (str \u001b (style ansi-styles)))
+
+(defn colorize
+  "Apply ANSI color to text."
+  [text color]
+  (str (ansi color) text (ansi :reset)))
 
 (defn render-pos
   [board pos]
-  nil)
+  (str (nth letters (dec pos))
+       (if (get-in board [pos :pegged])
+         (colorize "0" :blue)
+         (colorize "-" :red))))
 
 (defn row-positions
   "Return all positions in the given row."
   [row-num]
-  nil)
+  (range (inc (or (row-tri (dec row-num)) 0))
+         (inc (row-tri row-num))))
 
 (defn row-padding
   "String of spaces to add to the beginning of a row to center it."
   [row-num rows]
-  nil)
+  (let [pad-length (/ (* (- rows row-num) pos-chars) 2)]
+    (apply str (take pad-length (repeat " ")))))
 
 (defn render-row
   [board row-num]
-  nil)
+  (str (row-padding row-num (:rows board))
+       (clojure.string/join " " (map (partial render-pos board)
+                                     (row-positions row-num)))))
 
 (defn print-board
   [board]
-  nil)
+  (doseq [row-num (range 1 (inc (:rows board)))]
+    (println (render-row board row-num))))
 
 ;; player interaction
 ;;
