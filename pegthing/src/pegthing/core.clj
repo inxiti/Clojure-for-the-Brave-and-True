63516d39f30f340fe91f09e3da48f3ef95d5bf84
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-04 00:56:32 -0400

    finished chapter 5, pegthing

--- pegthing/src/pegthing/core.clj
+++ pegthing/src/pegthing/core.clj
@@ -184,7 +184,7 @@
                        (pegged? board jumped)))
                 (get-in board [pos :connections]))))
 
-(def my-board (assoc-in (new-board 5) [4 :pegged] false))
+;; (def my-board (assoc-in (new-board 5) [4 :pegged] false))
 ;; (valid-moves my-board 11) ;; => {4 7}
 
 (defn valid-move?
@@ -269,14 +269,82 @@
 ;; player interaction
 ;;
 
+(defn letter->pos
+  "Converts a letter string to the corresponding position number."
+  [letter]
+  (inc (- (int (first letter)) alpha-start)))
 
+(defn get-input
+  "Waits for user to enter text and hit enter, then cleans the input."
+  ([] (get-input nil))
+  ([default]
+   (let [input (clojure.string/trim (read-line))]
+     (if (empty? input)
+       default
+       (clojure.string/lower-case  input)))))
+
+(defn characters-as-strings
+  "Given a string, return a collection consisting of each individual character."
+  [string]
+  (re-seq #"[a-zA-Z]" string))
+
+;; (characters-as-strings "a   b") ;; => ("a" "b")
+
+(defn user-entered-invalid-move
+  "Handles the next step after a user has entered an invalid move."
+  [board]
+  (println "\n!!! That was an invalid move :(\n")
+  (prompt-move board))
+
+(defn user-entered-valid-move
+  "Handles the next step after a user has entered a valid move."
+  [board]
+  (if (can-move? board)
+    (prompt-move board)
+    (game-over board)))
+
+(defn prompt-move
+  [board]
+  (println "\nHere's your board: ")
+  (print-board board)
+  (println "Move from where to where? Enter two letters: ")
+  (let [input (map letter->pos (characters-as-strings (get-input)))]
+    (if-let [new-board (make-move board (first input) (second input))]
+      (user-entered-valid-move new-board)
+      (user-entered-invalid-move board))))
+
+(defn prompt-empty-peg
+  [board]
+  (println "Here's your board: ")
+  (print-board board)
+  (println "Remove which peg? [e] ")
+  (prompt-move (remove-peg board (letter->pos (get-input "e")))))
+
+(defn prompt-rows
+  []
+  (println "How many rows? [5] ")
+  (let [rows (Integer. (get-input 5))
+        board (new-board rows)]
+    (prompt-empty-peg board)))
+
+(defn game-over
+  "Announce the game is over and prompt the play again."
+  [board]
+  (let [remaining-pegs (count (filter :pegged (vals board)))]
+    (println "Game over! You had " remaining-pegs " pegs leg: ")
+    (print-board board)
+    (println "Play again? y/n [y] ")
+    (let [input (get-input "y")]
+      (if (= "y" input)
+        (prompt-rows)
+        (do
+          (println "Bye!")
+          (System/exit 0))))))
 ;; -----------------------------------------------------------------------------
 ;; main
 ;;
 
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
-  (if (empty? args)
-    (println (str "Hello, World!"))
-    (println (str "Hello, World!\n\targs: " args))))
+  []
+  (prompt-rows))
