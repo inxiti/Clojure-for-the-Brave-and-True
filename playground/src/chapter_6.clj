7a524cefb6eff0ca3754817f4fd9d89ee7b132f0
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-10 02:04:37 -0400

    new commenting style

--- playground/src/chapter_6.clj
+++ playground/src/chapter_6.clj
@@ -1,14 +1,19 @@
+;;; clojure for the brave and true
+;;;
+;;; chapter 6
+
 (ns chapter-6)
 
-; showing that symbols map to functions, variables, anything really. asdasdasd;lkasjdf;kljasdajsd
-; asdf
+;; demonstrating that symbols map to functions, variables, anything really.
+
 inc ; => function
 'inc ; => inc
 (map inc [1 2 3]) ; => [2 3 4]
 '(map inc [1 2 3]) ; => (map inc [1 2 3])
 
-(defn -main
-  []
-(println "Hello, world"))
+;; main
 
-(-main)
+(defn -main
+  [& args]
+  (println "Hello, world"))
+
