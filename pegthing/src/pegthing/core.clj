2efb9cfd8ef137dc9f3ad398224eca5efdc83f17
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-11 00:41:55 -0400

    updated commenting style

--- pegthing/src/pegthing/core.clj
+++ pegthing/src/pegthing/core.clj
@@ -1,28 +1,33 @@
+;;; clojure for the brave and true
+;;;
+;;; chapter 5
+
 (ns pegthing.core
   (require [clojure.set :as set])
   (:gen-class))
 
 ;; example of the board data structure
 ;;
-;;{1  {:pegged true, :connections {6 3, 4 2}},
-;; 2  {:pegged true, :connections {9 5, 7 4}},
-;; 3  {:pegged true, :connections {10 6, 8 5}},
-;; 4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
-;; 5  {:pegged true, :connections {14 9, 12 8}},
-;; 6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
-;; 7  {:pegged true, :connections {9 8, 2 4}},
-;; 8  {:pegged true, :connections {10 9, 3 5}},
-;; 9  {:pegged true, :connections {7 8, 2 5}},
-;; 10 {:pegged true, :connections {8 9, 3 6}},
-;; 11 {:pegged true, :connections {13 12, 4 7}},
-;; 12 {:pegged true, :connections {14 13, 5 8}},
-;; 13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
-;; 14 {:pegged true, :connections {12 13, 5 9}},
-;; 15 {:pegged true, :connections {13 14, 6 10}},
-;; :rows 5}
 
-;; declare allows you to declare functions without first defining them, allowing
-;; them to be referenced before they are defined.
+;{1  {:pegged true, :connections {6 3, 4 2}},
+; 2  {:pegged true, :connections {9 5, 7 4}},
+; 3  {:pegged true, :connections {10 6, 8 5}},
+; 4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
+; 5  {:pegged true, :connections {14 9, 12 8}},
+; 6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
+; 7  {:pegged true, :connections {9 8, 2 4}},
+; 8  {:pegged true, :connections {10 9, 3 5}},
+; 9  {:pegged true, :connections {7 8, 2 5}},
+; 10 {:pegged true, :connections {8 9, 3 6}},
+; 11 {:pegged true, :connections {13 12, 4 7}},
+; 12 {:pegged true, :connections {14 13, 5 8}},
+; 13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
+; 14 {:pegged true, :connections {12 13, 5 9}},
+; 15 {:pegged true, :connections {13 14, 6 10}},
+; :rows 5}
+
+; declare allows you to declare functions without first defining them, allowing
+; them to be referenced before they are defined.
 (declare successful-move prompt-move game-over query-rows)
 
 (defn tri*
@@ -32,28 +37,28 @@
    (let [new-sum (+ sum n)]
      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
 
-;; call the lazy sequence and bind it to tri
+; call the lazy sequence and bind it to tri
 (def tri (tri*))
 
-;; take, and thus realize, the first 5 elements of tri
-;; (take 5 tri) ;; => (1 3 6 10 15)
+; take, and thus realize, the first 5 elements of tri
+; (take 5 tri) ; => (1 3 6 10 15)
 
 (defn triangular?
   "Is the number triangular? e.g. 1, 3, 6, 10, 15, 21, etc"
   [n]
   (= n (last (take-while #(>= n %) tri))))
 
-;; (triangular? 5) ;; => false
-;; (triangular? 6) ;; => true
+; (triangular? 5) ; => false
+; (triangular? 6) ; => true
 
 (defn row-tri
   "The triangular number at the end of row n"
   [n]
   (last (take n tri)))
 
-;; (row-tri 1) ;; => 1
-;; (row-tri 2) ;; => 3
-;; (row-tri 3) ;; => 6
+; (row-tri 1) ; => 1
+; (row-tri 2) ; => 3
+; (row-tri 3) ; => 6
 
 (defn row-num
   "Returns row number the position belongs to: pos 1 in row 1,
@@ -61,8 +66,8 @@
   [pos]
   (inc (count (take-while #(> pos %) tri))))
 
-;; (row-num 1) ;; => 1
-;; (row-num 3) ;; => 5
+; (row-num 1) ; => 1
+; (row-num 3) ; => 5
 
 (defn connect
   "Form a mutual connection between two positions."
@@ -76,22 +81,24 @@
      [[pos destination] [destination pos]])
     board))
 
-;; [1, 4] and [4, 1] become {1 {:connections {4 2}} 4 {:connections {1 2}}}
+; [1, 4] and [4, 1] become {1 {:connections {4 2}} 4 {:connections {1 2}}}
 
-;; (connect {} 15 1 2 4) ;; => {1 {:connections {4 2}}
-                         ;;     4 {:connections {1 2}}}
+; (connect {} 15 1 2 4) ; => {1 {:connections {4 2}}
+                        ;     4 {:connections {1 2}}}
 
-;; examples of assoc-in: return a new map with the given value at the specified
-;; nesting. also of get-in: get a value within a nested associative structure.
+; examples of assoc-in: return a new map with the given value at the specified
+; nesting. also of get-in: get a value within a nested associative structure.
+
 (comment
   (assoc-in {} [:cookie :monster :vocals] "Finntroll")
-  ;; => {:cookie {:monster {:vocals "Finntroll"}}}
+  ; => {:cookie {:monster {:vocals "Finntroll"}}}
   (get-in {:cookie {:monster {:vocals "Finntroll"}}} [:cookie :monster])
-  ;; => {:vocals "Finntroll"}
+  ; => {:vocals "Finntroll"}
   (assoc-in {} [1 :connections 4] 2)
-  ;; => {1 {:connections {4 2}}}
+  ; => {1 {:connections {4 2}}}
 )
-;; connect two positions functions
+
+;; connect positions functions
 ;;
 
 (defn connect-right
@@ -117,8 +124,8 @@
         destination (+ 2 row neighbor)]
     (connect board max-pos pos neighbor destination)))
 
-;; (connect {} 15 1) ;; => {1 {:connections { 4 2}}  4 {:connections {1 2}}}
-;; (connect [} 15 3) ;; => {3 {:connections {10 6}} 10 {:connections {3 6}}}
+; (connect {} 15 1) ; => {1 {:connections { 4 2}}  4 {:connections {1 2}}}
+; (connect [} 15 3) ; => {3 {:connections {10 6}} 10 {:connections {3 6}}}
 
 (defn add-pos
   "Pegs the position and performs connections."
@@ -130,17 +137,17 @@
             pegged-board
             [connect-right connect-down-left connect-down-right])))
 
-;; (add-pos {} 15 1)
-;; => {1 {:pegged true, :connections {4 2, 6 3}},
-;;     4 {:connections {1 2}},
-;;     6 {:connections {1 3}}}
+; (add-pos {} 15 1)
+; => {1 {:pegged true, :connections {4 2, 6 3}},
+;     4 {:connections {1 2}},
+;     6 {:connections {1 3}}}
 
-;; using reduce to define clean from listing 5-1
-;; (defn clean
-;;   [text]
-;;   (reduce (fn [string string-fn] (string-fn string))
-;;           text
-;;           [clojure.string/trim #(clojure.string/replace % #"lol" "LOL")]))
+; using reduce to define clean from listing 5-1
+; (defn clean
+;   [text]
+;   (reduce (fn [string string-fn] (string-fn string))
+;           text
+;           [clojure.string/trim #(clojure.string/replace % #"lol" "LOL")]))
 
 (defn new-board
   "Creates a new board with the given number of rows."
@@ -154,6 +161,7 @@
 
 ;; moving pegs
 ;;
+
 (defn pegged?
   "Does the position have a peg in it?"
   [board pos]
@@ -184,16 +192,16 @@
                        (pegged? board jumped)))
                 (get-in board [pos :connections]))))
 
-;; (def my-board (assoc-in (new-board 5) [4 :pegged] false))
-;; (valid-moves my-board 11) ;; => {4 7}
+; (def my-board (assoc-in (new-board 5) [4 :pegged] false))
+; (valid-moves my-board 11) ; => {4 7}
 
 (defn valid-move?
   "Return jumped position if the move from p1 to p2 is valid, nil otherwise."
   [board p1 p2]
   (get (valid-moves board p1) p2))
 
-;; (valid-move? my-board 1 4) ;; => 2
-;; (valid-move? my-board 8 4) ;; => nil
+; (valid-move? my-board 1 4) ; => 2
+; (valid-move? my-board 8 4) ; => nil
 
 (defn make-move
   "Move peg from p1 to p2, reomving jumped peg."
@@ -201,20 +209,20 @@
   (if-let [jumped (valid-move? board p1 p2)]
     (move-peg (remove-peg board jumped) p1 p2)))
 
-;; using a ? indicates that the function is a predicate function, meant to be
-;; used in boolean expressions.
+; using a ? indicates that the function is a predicate function, meant to be
+; used in boolean expressions.
 (defn can-move?
   "Do any of the pegged positions have valid moves?"
   [board]
   (some (comp not-empty (partial valid-moves board))
         (map first (filter #(get (second %) :pegged) board))))
 
-;; (can-move? my-board) ;; => {4 2}
+; (can-move? my-board) ; => {4 2}
 
 ;; rendering, and printing the board
 ;;
 
-;; put all lower-case alphabetical letters in a (lazy) sequence
+; put all lower-case alphabetical letters in a (lazy) sequence
 (def alpha-start 97)
 (def alpha-end 123)
 (def letters (map (comp str char) (range alpha-start alpha-end)))
@@ -288,7 +296,7 @@
   [string]
   (re-seq #"[a-zA-Z]" string))
 
-;; (characters-as-strings "a   b") ;; => ("a" "b")
+; (characters-as-strings "a   b") ; => ("a" "b")
 
 (defn user-entered-invalid-move
   "Handles the next step after a user has entered an invalid move."
@@ -340,6 +348,7 @@
         (do
           (println "Bye!")
           (System/exit 0))))))
+
 ;; -----------------------------------------------------------------------------
 ;; main
 ;;
@@ -348,3 +357,4 @@
   "I don't do a whole lot ... yet."
   []
   (prompt-rows))
+
