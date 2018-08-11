9263fe8dbe2e42af966fa4bf81277956b54ea925
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-11 00:42:44 -0400

    commenting style updated. fixed a typo

--- playground/src/chapter_6.clj
+++ playground/src/chapter_6.clj
@@ -5,6 +5,7 @@
 (ns chapter-6)
 
 ;; demonstrating that symbols map to functions, variables, anything really.
+;;
 
 inc ; => function
 'inc ; => inc
@@ -13,12 +14,13 @@ inc ; => function
 
 ; this process is called interning a var
 (def great-books ["East of Eden" "The Glass Bead Game"])
+
 ; => #'chapter-6/great-books
 
 ; get a map of the interned vars
 (ns-interns *ns*)
 
-(get (ns-interns *ns) 'great-books) ; => #'chapter-6/great-books
+(get (ns-interns *ns*) 'great-books) ; => #'chapter-6/great-books
 
 ; #' grabs hold of the var corresponding to the symbol that follows it.
 ; deref vars to get the objects they point to.
@@ -31,7 +33,9 @@ great-books ; ["East of Eden" "The Glass Bead Game"]
 ; avoid it. the following overwrites the previous great-books value.
 (def great-books ["The Power of Bees" "Journey to Upstairs"])
 
+;; -----------------------------------------------------------------------------
 ;; main
+;;
 
 (defn -main
   [& args]
