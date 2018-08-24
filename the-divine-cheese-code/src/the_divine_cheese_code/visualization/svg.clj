db3b82301523236bf47078477def01af3b84ed02
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-24 00:08:01 -0400

    fns added to svg.clj

--- the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
+++ the-divine-cheese-code/src/the_divine_cheese_code/visualization/svg.clj
@@ -1,4 +1,15 @@
-(ns the-divine-cheese-code.visualization.svg)
+(ns the-divine-cheese-code.visualization.svg
+  (:require [clojure.string :as s])
+  (:refer-clojure :exclude [min max]))
+
+(defn comparator-over-maps
+  [comparison-fn ks]
+  (fn [maps]
+    (zipmap ks
+            (map (fn [k] (apply comparison-fn (map k maps)))))))
+
+(def min (comparator-over-maps clojure.core/min [:lat :lng]))
+(def max (comparator-over-maps clojure.core/max [:lat :lng]))
 
 (defn latlng->point
   "Convert lat/lng map to comma-separated string."
