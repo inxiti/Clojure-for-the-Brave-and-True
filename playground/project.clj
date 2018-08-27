39f79115623091de807778e3dd01d3546f71d8bd
inxiti <32788205+inxiti@users.noreply.github.com>
2018-08-27 00:26:36 -0400

    changed the main entry point

--- playground/project.clj
+++ playground/project.clj
@@ -1,6 +1,6 @@
 (defproject playground "0.1.0-SNAPSHOT"
-  :main chapter-6
-  :description "A playground for me to test ideas and experiment with Clojure."
+  :main chapter-7
+  :description "A playground for me to test ideas, and experiment with Clojure."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
