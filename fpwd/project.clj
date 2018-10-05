ead4685f5704c9461335ad2f585a60aaac4ec8d2
inxiti <32788205+inxiti@users.noreply.github.com>
2018-10-05 01:44:15 -0400

    Removed license

--- fpwd/project.clj
+++ fpwd/project.clj
@@ -1,7 +1,5 @@
 (defproject fpwd "0.1.0-SNAPSHOT"
-  :description "fpwd"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :description "fpwd from clojure for the brave and true"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot fpwd.core
   :target-path "target/%s"
