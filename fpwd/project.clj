b02ea764c1571526a262899a4128dc243e26f0f9
inxiti <32788205+inxiti@users.noreply.github.com>
2018-10-06 03:08:34 -0400

    update desc

--- fpwd/project.clj
+++ fpwd/project.clj
@@ -1,5 +1,5 @@
 (defproject fpwd "0.1.0-SNAPSHOT"
-  :description "fpwd from clojure for the brave and true"
+  :description "fpwd, from clojure for the brave and true"
   :dependencies [[org.clojure/clojure "1.8.0"]]
   :main ^:skip-aot fpwd.core
   :target-path "target/%s"
